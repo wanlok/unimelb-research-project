2d8990194608e44278bfe54ec3fe721e491ca212
Tung Lok Wan <tunglokw@student.unimelb.edu.au>
2023-03-01 15:05:16 +1100

    commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# https://api.github.com/users/wanlok/repos
+# https://api.github.com/repos/wanlok/chatbot-web
+# https://api.github.com/repos/wanlok/chatbot-web/contents
+
+if __name__ == '__main__':
+    print('Hello World')
