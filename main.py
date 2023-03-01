faf45477dcd0359648cfdf5f438f7c9a73f6f5fe
Tung Lok Wan <tunglokw@student.unimelb.edu.au>
2023-03-01 22:30:48 +1100

    added support to store history in csv file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import json
 import random
 from urllib.error import HTTPError
@@ -7,13 +8,15 @@ from urllib.request import Request, urlopen
 def get_json(url_string, is_array=True):
     request = Request(url_string)
     try:
+        print(f'url: {url_string}')
         response = urlopen(request)
-        print(f'count: {response.info()["X-RateLimit-Remaining"]}')
+        rate_limit_remaining = response.info()["X-RateLimit-Remaining"]
         json_response = json.loads(response.read())
     except HTTPError as err:
         print(f'error: {err.reason}')
+        rate_limit_remaining = 1
         json_response = {} if is_array else []
-    return json_response
+    return rate_limit_remaining, json_response
 
 
 if __name__ == '__main__':
@@ -21,12 +24,29 @@ if __name__ == '__main__':
     # https://api.github.com/users/wanlok/repos
     # https://api.github.com/repos/wanlok/chatbot-web
     # https://api.github.com/repos/wanlok/chatbot-web/contents
-    since = random.randint(0, 100000000)
+    csv_file_path = 'C:\\Files\\Projects\\unimelb-research-project\\repositories.csv'
+    read_file = open(csv_file_path)
+    reader = csv.reader(read_file, delimiter=',')
+    old_repository_names = set()
+    for row in reader:
+        old_repository_names.add(row[0])
+    write_file = open(csv_file_path, 'a', newline='')
+    writer = csv.writer(write_file)
+    if len(old_repository_names) == 0:
+        writer.writerow(['Name', 'Files'])
+    since = random.randint(320000000, 330000000)
     print(f'since: {since}')
-    repositories = get_json(f'https://api.github.com/repositories?since={since}')
-    for repository in repositories:
-        repository_name = repository['full_name']
-        print(f'name: {repository_name}')
-        contents = get_json(f'https://api.github.com/repos/{repository_name}/contents')
+    _, repositories = get_json(f'https://api.github.com/repositories?since={since}')
+    new_repository_names = set(map(lambda j: j['full_name'], repositories))
+    repository_names = new_repository_names - old_repository_names
+    print(f'new: {len(new_repository_names)}')
+    print(f'old: {len(old_repository_names)}')
+    print(f'new - old: {len(repository_names)}')
+    for repository_name in repository_names:
+        count, contents = get_json(f'https://api.github.com/repos/{repository_name}/contents')
         file_names = list(map(lambda j: j['name'], contents))
-        print(f'files: {file_names}')
+        if len(file_names) > 0:
+            writer.writerow([repository_name, file_names])
+        if count == 0:
+            break
+
