a4b3e07bd452e4e735f43fee62f134766359b21c
Tung Lok Wan <tunglokw@student.unimelb.edu.au>
2023-03-07 15:30:05 +1100

    updated pbi dashboard

--- crawler_6.py
+++ crawler_6.py
@@ -1,3 +1,5 @@
+from urllib.parse import quote
+
 from utils import csv_writer, csv_reader, prepare_csv_file, get_json, get_content, get_csv_start_index
 
 
@@ -10,13 +12,16 @@ if __name__ == '__main__':
     content_csv_writer = csv_writer(content_csv_file_path)
     content_csv_reader = csv_reader(content_csv_file_path)
     content_csv_rows = prepare_csv_file(content_csv_reader, content_csv_writer, ['repo', 'path', 'content'])
-    for i in range(get_csv_start_index(files_csv_rows, content_csv_rows, 2), len(files_csv_rows)):
-        repo = files_csv_rows[i][0]
-        path = files_csv_rows[i][1]
+    start_index = get_csv_start_index(files_csv_rows, content_csv_rows, 2)
+    print(f'{start_index} {len(files_csv_rows)}')
+    while start_index < len(files_csv_rows):
+        repo = quote(files_csv_rows[start_index][0])
+        path = quote(files_csv_rows[start_index][1])
         content = get_json(f'https://api.github.com/repos/{repo}/contents/{path}')
-        print(content)
-        download_url = ['download_url']
-        row = [repo, path, get_content(download_url)]
-        if row not in content_csv_rows:
-            content_csv_writer.writerow(row)
-            content_csv_rows.append(row)
+        if content is not None:
+            download_url = content['download_url']
+            row = [repo, path, get_content(download_url)]
+            if row not in content_csv_rows:
+                content_csv_writer.writerow(row)
+                content_csv_rows.append(row)
+        start_index = start_index + 1
