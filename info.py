a282f700163c611148329e536b0972ba234e5bb4
Tung Lok Wan <tunglokw@student.unimelb.edu.au>
2023-03-08 03:14:47 +1100

    updated power bi dashboard

--- info.py
+++ info.py
@@ -1,29 +1,19 @@
 import re
 
-from utils import csv_reader
+from utils import csv_reader, csv_writer, prepare_csv_file
 
 if __name__ == '__main__':
     content_csv_file_path = 'content.csv'
     content_csv_reader = csv_reader(content_csv_file_path)
-
-    dict = dict()
+    info_csv_file_path = 'info.csv'
+    info_csv_writer = csv_writer(info_csv_file_path)
+    info_csv_reader = csv_reader(info_csv_file_path)
+    info_csv_rows = prepare_csv_file(info_csv_reader, info_csv_writer, ['repo', 'path', 'number_of_headings'])
+    i = 0
     for row in content_csv_reader:
-        key = f'{row[0]} {row[1]}'
-        if key in dict:
-            dict[key] = dict[key] + 1
-        else:
-            dict[key] = 1
-    for a in dict:
-        if dict[a] > 1:
-            print(f'{a} {dict[a]}')
-
-
-
-    # i = 9999
-    # titles = set()
-    # list
-    # for row in content_csv_reader:
-    #     lll = re.findall(r'#+ *(.*?)\\n', row[2])
-    #     # print(f'{row[0]} {row[1]} {lll}')
-    #     titles.update(lll)
-    # print(titles)
+        if i > 0:
+            if row[1] == 'doc/development/fe_guide/security.md':
+                headings = re.findall(r'[b\'|b\"|\\n]#+ *(.*?) *\\n', row[2])
+                print(f'{row[0]} {headings}')
+            # info_csv_writer.writerow([row[0], row[1], len(headings)])
+        i = i + 1
