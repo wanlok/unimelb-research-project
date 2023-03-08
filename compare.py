f91e8176c9612a4b1ddc5ecb9260c5d950c212be
Tung Lok Wan <tunglokw@student.unimelb.edu.au>
2023-03-08 20:38:28 +1100

    added levenshtein distance to the compare program

--- compare.py
+++ compare.py
@@ -1,6 +1,8 @@
 import sys
 
-from utils import csv_reader, csv_writer
+from Levenshtein import distance
+
+from utils import csv_reader, csv_writer, prepare_csv_file
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -8,11 +10,13 @@ if __name__ == '__main__':
         content_csv_reader = csv_reader(content_csv_file_path)
         compare_csv_file_path = 'compare.csv'
         compare_csv_writer = csv_writer(compare_csv_file_path, mode='w')
+        compare_csv_reader = csv_reader(compare_csv_file_path)
+        compare_csv_rows = prepare_csv_file(compare_csv_reader, compare_csv_writer, ['dominant_content', 'content', 'levenshtein_distance', 'occurrence'])
         content_dict = {}
         i = 0
         for row in content_csv_reader:
             if i > 0:
-                if sys.argv[1] in row[2]:
+                if sys.argv[1].lower() in row[2].lower():
                     if row[2] in content_dict:
                         content_dict[row[2]] = content_dict[row[2]] + 1
                     else:
@@ -29,4 +33,4 @@ if __name__ == '__main__':
         for key in content_dict:
             content = key.replace('\\n', '\n')
             if content != dominant_content:
-                compare_csv_writer.writerow([dominant_content, content, content_dict[key]])
+                compare_csv_writer.writerow([dominant_content, content, distance(dominant_content, content), content_dict[key]])
