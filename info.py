d13709b33ab8d4f1d0e07d748d6788aae232035d
Tung Lok Wan <tunglokw@student.unimelb.edu.au>
2023-03-09 03:02:49 +1100

    added language classification

--- info.py
+++ info.py
@@ -1,5 +1,7 @@
 import re
 
+import langid
+
 from utils import csv_reader, csv_writer, prepare_csv_file
 
 if __name__ == '__main__':
@@ -8,10 +10,11 @@ if __name__ == '__main__':
     info_csv_file_path = 'info.csv'
     info_csv_writer = csv_writer(info_csv_file_path, mode='w')
     info_csv_reader = csv_reader(info_csv_file_path)
-    info_csv_rows = prepare_csv_file(info_csv_reader, info_csv_writer, ['repo', 'path', 'number_of_headings'])
+    info_csv_rows = prepare_csv_file(info_csv_reader, info_csv_writer, ['repo', 'path', 'number_of_headings', 'language'])
     i = 0
     for row in content_csv_reader:
         if i > 0:
             headings = re.findall(r'[b\'|b\"|\\n]#+ *(.*?) *\\n', row[2])
-            info_csv_writer.writerow([row[0], row[1], len(headings)])
+            language = langid.classify(eval(row[2]).decode('utf-8'))[0].upper()
+            info_csv_writer.writerow([row[0], row[1], len(headings), language])
         i = i + 1
