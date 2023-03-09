3073cbd6f1729213e9ab46588b28f1d7995c1968
Tung Lok Wan <tunglokw@student.unimelb.edu.au>
2023-03-09 13:30:22 +1100

    added cve count

--- info.py
+++ info.py
@@ -7,14 +7,16 @@ from utils import csv_reader, csv_writer, prepare_csv_file
 if __name__ == '__main__':
     content_csv_file_path = 'content.csv'
     content_csv_reader = csv_reader(content_csv_file_path)
-    info_csv_file_path = 'info.csv'
-    info_csv_writer = csv_writer(info_csv_file_path, mode='w')
-    info_csv_reader = csv_reader(info_csv_file_path)
-    info_csv_rows = prepare_csv_file(info_csv_reader, info_csv_writer, ['repo', 'path', 'number_of_headings', 'language'])
+    content_info_csv_file_path = 'info.csv'
+    content_info_csv_writer = csv_writer(content_info_csv_file_path, mode='w')
+    content_info_csv_reader = csv_reader(content_info_csv_file_path)
+    content_info_csv_rows = prepare_csv_file(content_info_csv_reader, content_info_csv_writer, ['repo', 'path', 'number_of_headings', 'language', 'cve_count'])
     i = 0
     for row in content_csv_reader:
         if i > 0:
+            content = eval(row[2]).decode('utf-8')
             headings = re.findall(r'[b\'|b\"|\\n]#+ *(.*?) *\\n', row[2])
-            language = langid.classify(eval(row[2]).decode('utf-8'))[0].upper()
-            info_csv_writer.writerow([row[0], row[1], len(headings), language])
+            cve_count = content.lower().count('cve')
+            language = langid.classify(content)[0].upper()
+            content_info_csv_writer.writerow([row[0], row[1], len(headings), language, cve_count])
         i = i + 1
