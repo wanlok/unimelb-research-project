a7e4e7d03d6313f9d3b77785e323d59ba0ff65b2
Tung Lok Wan <tunglokw@student.unimelb.edu.au>
2023-03-10 01:15:56 +1100

    updated crawler 7 to use code api

--- crawler_7.py
+++ crawler_7.py
@@ -1,12 +1,29 @@
-import subprocess
 import time
 
+from utils import get_secret, csv_reader, csv_writer, prepare_csv_file, get_json
+
 if __name__ == '__main__':
+    secret = get_secret()
+    files_csv_file_path = 'files.csv'
+    files_csv_writer = csv_writer(files_csv_file_path)
+    files_csv_reader = csv_reader(files_csv_file_path)
+    files_csv_rows = prepare_csv_file(files_csv_reader, files_csv_writer, ['repo', 'path', 'url'])
+    q = 'security+in:path+language:markdown'
+    per_page = 100
+    page = 1
+    size_limit = 1000
+    time_limit = 60 + 3
     first = True
     while True:
-        if not first:
-            time.sleep(60 + 3)
-        subprocess.Popen(['python', 'crawler_2.py']).wait()
-        time.sleep(60 + 3)
-        subprocess.Popen(['python', 'crawler_5.py']).wait()
-        first = False
+        while page * per_page <= size_limit:
+            if not first:
+                time.sleep(time_limit)
+            first = False
+            codes = get_json(f'https://api.github.com/search/code?q={q}&per_page={per_page}&page={page}', token=secret['personal_access_token'])
+            if codes is not None:
+                for code in codes['items']:
+                    row = [code['repository']['full_name'], code['path'], code['html_url']]
+                    if row not in files_csv_rows:
+                        files_csv_writer.writerow(row)
+                        files_csv_rows.append(row)
+                page = page + 1
