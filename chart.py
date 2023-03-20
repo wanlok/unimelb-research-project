9543136882f179386064c87a474d7297696479a8
Tung Lok Wan <tunglokw@student.unimelb.edu.au>
2023-03-21 03:57:49 +1100

    added chart generation feature in cvss

--- chart.py
+++ chart.py
@@ -1,22 +1,62 @@
 from math import floor, ceil
 
+import numpy as np
 from matplotlib import pyplot as plt
 
+font_name = 'Times New Roman'
+font = {'fontname': font_name}
+padding_1 = 16
+padding_2 = 24
 
 def plot(title, x, y, file_path):
-    font_name = 'Times New Roman'
-    font = {'fontname': font_name}
     fig = plt.figure()
     fig.set_size_inches(len(x) * 2, 8)
     plt.bar(x, y, color='black', width=0.24)
     plt.xticks(rotation=45, fontname=font_name)
-    plt.xlabel('SECURITY.md Update Dates', fontdict=font, labelpad=24)
+    plt.xlabel('SECURITY.md Update Dates', fontdict=font, labelpad=padding_2)
     plt.yticks(range(floor(min(y)), ceil(max(y)) + 1, 50), fontname=font_name)
-    plt.ylabel('Number of CVEs Before Updates', fontdict=font, labelpad=24)
-    plt.title(title, fontdict=font, pad=24)
+    plt.ylabel('Number of CVEs Before Updates', fontdict=font, labelpad=padding_2)
+    plt.title(title, fontdict=font, pad=padding_2)
     ax = plt.gca()
     for i in range(len(y)):
         rect = ax.patches[i]
         ax.text(rect.get_x() + rect.get_width() / 2, y[i], f'{y[i]}\n', ha='center', fontdict=font)
+    plt.savefig(file_path, dpi=300, bbox_inches='tight')
+    plt.close()
+
+def plot2(title, date_list, data, file_path):
+    weight_counts = dict()
+    length = 0
+    for i in range(len(data)):
+        length = sum(data[i])
+        for j in range(10):
+            if j in weight_counts:
+                weight_counts[j].append(data[i][j])
+            else:
+                weight_counts[j] = [data[i][j]]
+    fig = plt.figure()
+    fig.set_size_inches(len(date_list) * 1, 8)
+    length = length + (length * 0.04)
+    width = 0.56
+    species = list(map(lambda x: f'{x}', date_list))
+    ax = plt.gca()
+    bottom = np.zeros(len(species))
+    for j, weight_count in weight_counts.items():
+        if j == 9:
+            label = f'9.0 - 10.0'
+        else:
+            label = f'{j}.0 - {j + 0.9}'
+        ax.bar(species, weight_count, width, label=label, bottom=bottom)
+        bottom += weight_count
+    for container in ax.containers:
+        labels = [int(value) if value > 0 else '' for value in container.datavalues]
+        ax.bar_label(container, labels=labels, label_type='center', **font)
+    plt.xticks(font=font_name)
+    plt.yticks(font=font_name)
+    ax.tick_params(axis='x', pad=padding_1)
+    ax.tick_params(axis='y', pad=padding_1)
+    plt.title(title, fontdict=font, pad=padding_1)
+    plt.setp(ax.legend(loc='upper left').texts, family=font_name)
+    ax.set_ylim([0, length])
     plt.savefig(file_path, dpi=300, bbox_inches='tight')
     plt.close()
--- cvss.py
+++ cvss.py
@@ -1,7 +1,8 @@
 import sys
 
+import chart
 import nvdcve
-
+from utils import csv_writer, csv_reader, prepare_csv_file
 
 rating_names = ['Low', 'Medium', 'High', 'Critical', 'None']
 
@@ -53,14 +54,17 @@ def number(my_list):
     return my_list
 
 
-if __name__ == '__main__':
-    repo = sys.argv[1]
+def generate_charts(repo):
     cve_list = nvdcve.get_list(repo)
     date_dict = dict()
     for cve in cve_list:
         date_dict[int(cve[1][:-2])] = 1
     date_list = list(date_dict.keys())
     date_list.sort()
+    v2_i = []
+    v2_e = []
+    v3_i = []
+    v3_e = []
     for date in date_list:
         cve_list = nvdcve.get_list(repo, f'{date}99')
         v2_i_rating_list = [0, 0, 0, 0, 0]
@@ -81,7 +85,32 @@ if __name__ == '__main__':
             increment_rating(v3_e_rating_list, get_v3_rating(cve[5]))
             increment_number(v3_e_number_list, cve[5])
         print(f'{date}')
-        print(f'Version 2 Impact        : {number(v2_i_number_list)} {rating(v2_i_rating_list)}')
-        print(f'Version 2 Explotability : {number(v2_e_number_list)} {rating(v2_e_rating_list)}')
-        print(f'Version 3 Impact        : {number(v3_i_number_list)} {rating(v3_i_rating_list)}')
-        print(f'Version 3 Explotability : {number(v3_e_number_list)} {rating(v3_e_rating_list)}')
+        print(f'Version 2 Impact        : {v2_i_number_list} {rating(v2_i_rating_list)}')
+        print(f'Version 2 Explotability : {v2_e_number_list} {rating(v2_e_rating_list)}')
+        print(f'Version 3 Impact        : {v3_i_number_list} {rating(v3_i_rating_list)}')
+        print(f'Version 3 Explotability : {v3_e_number_list} {rating(v3_e_rating_list)}')
+        v2_i.append(v2_i_number_list)
+        v2_e.append(v2_e_number_list)
+        v3_i.append(v3_i_number_list)
+        v3_e.append(v3_e_number_list)
+    file_name = '_'.join(repo.split('/'))
+    chart.plot2(f'{repo} - CVSS v2 Impact Score Distribution', date_list, v2_i, f'cvss/{file_name}_v2_i.png')
+    chart.plot2(f'{repo} - CVSS v2 Exploitability Score Distribution', date_list, v2_e, f'cvss/{file_name}_v2_e.png')
+    chart.plot2(f'{repo} - CVSS v3 Impact Score Distribution', date_list, v3_i, f'cvss/{file_name}_v3_i.png')
+    chart.plot2(f'{repo} - CVSS v3 Exploitability Score Score Distribution', date_list, v3_e, f'cvss/{file_name}_v3_e.png')
+
+
+if __name__ == '__main__':
+    repo_file_path = 'repo.csv'
+    repo_writer = csv_writer(repo_file_path)
+    repo_reader = csv_reader(repo_file_path)
+    repo_rows = prepare_csv_file(repo_reader, repo_writer, ['repo'])
+    length = len(repo_rows)
+    start = 1
+    end = 50
+    if end > length:
+        end = length
+    for i in range(1, len(repo_rows)):
+        repo = repo_rows[i][0]
+        print(f'{i} {repo}')
+        generate_charts(repo)
