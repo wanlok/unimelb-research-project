81d883b440f564e9b62c4e709391be30546c307a
Tung Lok Wan <tunglokw@student.unimelb.edu.au>
2023-07-08 13:09:49 +1000

    dummy program to find what repo do not have complete issue list downloaded

--- chart_dummy.py
+++ chart_dummy.py
@@ -1,8 +1,102 @@
-import repository
+import os
+from datetime import datetime
 
-security_md_directory_path = f'C:\\Files\\Projects\\wanlok.github.io\\research\\data\\securities\\'
+from dateutil.relativedelta import relativedelta
+
+import chart
+import nvdcve
+import repository
+from security_md import get_date_statistics, directory_path, get_date_statistics_2
+from utils import get_start_and_end_date_string, csv_reader
+
+
+def get_repositories_by_language(target_language):
+    repos = set()
+    for row in csv_reader('C:\\Users\\WAN Tung Lok\\Desktop\\NVD Repositories with SECURITY.md.csv'):
+        exists = False
+        for language in eval(row[3]):
+            if language.lower() == target_language.lower():
+                exists = True
+                break
+        if exists:
+            repo = row[0]
+            file_name = '_'.join(repo.split('/'))
+            file_path = f'C:\\Files\\a\\{file_name}.csv'
+            if os.path.exists(file_path):
+                repos.add(repo)
+    for row in csv_reader('C:\\Users\\WAN Tung Lok\\Desktop\\Random Repositories.csv'):
+        exists = False
+        for language in eval(row[3]):
+            if language.lower() == target_language.lower():
+                exists = True
+                break
+        if exists:
+            repo = row[0]
+            file_name = '_'.join(row[0].split('/'))
+            file_path = f'C:\\Files\\a\\{file_name}.csv'
+            if os.path.exists(file_path):
+                repos.add(repo)
+    return list(repos)
+
+
 
 if __name__ == '__main__':
-    print('hihi')
-    for repo in repository.get_list(10):
-        print(repo)
+    start_date = 20210101
+    end_date = 20211231
+    title = 'Number of java repositories with SECURITY.md updates per day in 2023'
+    file_path = f'C:\\Files\\Projects\\wanlok.github.io\\research\\charts\\2023.png'
+    repos = get_repositories_by_language('java')
+    # repos = repository.get_list_by_keywords(keywords=['java'], not_keywords=['javascript'])
+    # repos = repository.get_list_by_keywords(keywords=['python'])
+    chart.dummy(repos, start_date, end_date, title, file_path)
+
+    # print(repos)
+
+    # for year in range(2010, 2024):
+    #     repository.download_yearly_issue_counts(repos, year)
+    # issue_count = repository.get_issue_count('looly/hutool')
+
+    # for repo in repos:
+    #     for row in repository.get_issue_count_rows(repo, start_date, end_date):
+    #         print(row)
+
+
+    # repo = 'chatopera/cskefu'
+    # # for month in range(1, 12):
+    # #     month = '{:02d}'.format(month)
+    # #     start_date, end_date = get_start_and_end_date_string(f'2020{month}01')
+    # #     # number_of_cves = len(nvdcve.get_list(repo, start_date, end_date))
+    # #     number_of_issues = repository.get_issue_count_rows(repo, start_date, end_date)[0][2]
+    # #     print(f'{start_date} {end_date} {number_of_issues}')
+    # print(nvdcve.get_list(repo))
+    # for month in range(1, 10):
+    #     month = '{:02d}'.format(month)
+    #     start_date, end_date = get_start_and_end_date_string(f'2021{month}01')
+    #     number = len(nvdcve.get_list(repo, start_date, end_date))
+    #     # number = repository.get_issue_count_rows(repo, start_date, end_date)[0][2]
+    #     print(f'{2021}{month} {number}')
+
+    # nvdcve.get_list()
+
+
+    # for repo in repos:
+    #     repo_file_name = '_'.join(repo.split('/'))
+    #     repo_file_path = f'{directory_path}{repo_file_name}.csv'
+    #     _, date_dict = get_date_statistics_2(repo_file_path, start_date, end_date)
+    #     if len(date_dict) > 0:
+    #         print(f'{repo}')
+    #         for date in date_dict:
+    #             print(date)
+    #             for bcompare in date_dict[date]:
+    #                 print(bcompare)
+
+
+
+    # year = 2022
+    # for month in range(1, 13):
+    #     month = '{:02d}'.format(month)
+    #     start_date, end_date = get_start_and_end_date_string(f'{year}{month}01')
+    #     # start_date = 20220302
+    #     # end_date = 20220302
+    #     # print(f'{year}{month} {get_repo_statistics(start_date, end_date)}')
+    #     print(f'{year}{month} {len(get_repo_statistics(start_date, end_date))}')
--- dummy.py
+++ dummy.py
@@ -1,9 +1,72 @@
 import os
 
+import numpy as np
+import pandas as pd
+
 import repository
 import security_md
+from document.document_attribute_download import e_list
 from utils import csv_reader
+
+security_related_keywords = ['access policy', 'access role', 'access-policy', 'access-role', 'accesspolicy', 'accessrole', 'aes', 'audit', 'authentic', 'authority', 'authoriz', 'biometric', 'black list', 'black-list', 'blacklist', 'blacklist', 'cbc', 'certificate', 'checksum', 'cipher', 'clearance', 'confidentiality', 'cookie', 'crc', 'credential', 'crypt', 'csrf', 'decode', 'defensive programming', 'defensive-programming', 'delegation', 'denial of service', 'denial-of-service', 'diffie-hellman', 'dmz', 'dotfuscator', 'dsa', 'ecdsa', 'encode', 'escrow', 'exploit', 'firewall', 'forge', 'forgery', 'gss api', 'gss-api', 'gssapi', 'hack', 'hash', 'hmac', 'honey pot', 'honey-pot', 'honeypot', 'inject', 'integrity', 'kerberos', 'ldap', 'login', 'malware', 'md5', 'nonce', 'nss', 'oauth', 'obfuscat', 'open auth', 'open-auth', 'openauth', 'openid', 'owasp', 'password', 'pbkdf2', 'pgp', 'phishing', 'pki', 'privacy', 'private key', 'private-key', 'privatekey', 'privilege', 'public key', 'public-key', 'publickey', 'rbac', 'rc4', 'repudiation', 'rfc 2898', 'rfc-2898', 'rfc2898', 'rijndael', 'rootkit', 'rsa', 'salt', 'saml', 'sanitiz', 'secur', 'sha', 'shell code', 'shell-code', 'shellcode', 'shibboleth', 'signature', 'signed', 'signing', 'sing sign-on', 'single sign on', 'single-sign-on', 'smart assembly', 'smart-assembly', 'smartassembly', 'snif', 'spam', 'spnego', 'spoofing', 'spyware', 'ssl', 'sso', 'steganography', 'tampering', 'trojan', 'trust', 'violat', 'virus', 'white list', 'white-list', 'whitelist', 'x509', 'xss']
+
+
+def files_and_repos(directory_path, file_extension, function):
+    for file_name in os.listdir(directory_path):
+        repo = file_name.replace(f'.{file_extension}', '', -1).replace('_', '/', 1)
+        function(directory_path, file_name, repo)
+
+
+def get_repo_issues(directory_path, file_name, repo):
+    with open(f'{directory_path}{file_name}', encoding='utf-8') as f:
+        issues = eval(f.readlines()[0])
+        number_of_issues = len(issues)
+        issue_types = np.zeros(number_of_issues)
+        for i in range(number_of_issues):
+            created_at = issues[i]['node']['createdAt']
+            comments = issues[i]['node']['comments']['edges']
+            number_of_comments = len(comments)
+            comment_types = np.zeros(number_of_comments)
+            for j in range(number_of_comments):
+                for text in comments[j]['node']['bodyText'].split(' '):
+                    text_length = len(text)
+                    if text_length == 3:
+                        for keyword in security_related_keywords:
+                            if keyword == text:
+                                comment_types[j] = 1
+                                break
+                    elif text_length > 3:
+                        for keyword in security_related_keywords:
+                            if keyword in text:
+                                comment_types[j] = 1
+                                break
+            if number_of_comments > 0 and len([x for x in comment_types if x == 1]) / number_of_comments > 0.5:
+                issue_types[i] = 1
+        print(f'{repo} {number_of_issues} {len([x for x in issue_types if x == 1]) }')
+
+
+def ddd(directory_path, file_name, repo):
+    count_dict = dict()
+    for row in csv_reader('C:\\Users\\WAN Tung Lok\\Desktop\\repo_issue_counts.txt'):
+        repo = row[0]
+        if row[1] != 'failed':
+            count = int(row[1])
+        else:
+            count = None
+        if count is not None:
+            count_dict[repo] = count
+    with open(f'{directory_path}{file_name}', encoding='utf-8') as f:
+        issues = eval(f.readlines()[0])
+        if repo in count_dict:
+            count = count_dict[repo]
+            if count != len(issues) and count < 5000 and file_name in e_list:
+                print(f'\'{file_name}\',')
+        # else:
+        #     print(f'{file_name} None')
 
 
 if __name__ == '__main__':
-    print('Hello World')
+    directory_path = 'C:\\Files\\issues\\'
+    file_extension = 'txt'
+    # files_and_repos(, 'txt', get_repo_issues)
+    files_and_repos(directory_path, file_extension, ddd)
