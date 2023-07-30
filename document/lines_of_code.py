6f8f4ac1de578d2a0c338092ae1af44abe556881
Robert Wan <wantunglok@gmail.com>
2023-07-31 03:16:56 +1000

    updated lines of code logic

--- document/lines_of_code.py
+++ document/lines_of_code.py
@@ -1,9 +1,25 @@
+import os
+import subprocess
+
+cloc_directory_path = 'C:\\Files\\Projects\\cloc\\'
+os.chdir(cloc_directory_path)
+
+
 from utils import repos
 
 
 def do_something(repo):
-    print(repo)
+    file_name = repo.replace('/', '_')
+    file_name = f'{file_name}.txt'
+    file_path = f'{cloc_directory_path}{file_name}'
+    if not os.path.exists(file_path):
+        print(f'{repo} {file_path}')
+        p = subprocess.Popen(['cloc-git', repo], stdout=subprocess.PIPE, shell=True)
+        content, _ = p.communicate()
+        with open(file_path, 'w') as f:
+            f.write(content.decode())
 
 
 if __name__ == '__main__':
     repos(do_something)
+    # do_something('wanlok/dummy')
